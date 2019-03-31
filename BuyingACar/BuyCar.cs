c7a4dd13d940256a18dd45c170b35fd74e61ba0a
gary.lai99 <gary.lai99@TI03NB133>
2019-03-31 17:12:29 +0800

    Add TestMethod"OldPrice_2000_NewPrice_8000_At_First_Month_Will_Return_Month_1_Left_Nevigate4910"

--- BuyingACar/BuyCar.cs
+++ BuyingACar/BuyCar.cs
@@ -10,7 +10,21 @@ namespace BuyingACar
     {
         public static int[] NbMonths(int startPriceOld, int startPriceNew, int savingPerMonth, double percentLossByMonth)
         {
-            return new[] { 0, startPriceOld - startPriceNew };
+            if (startPriceOld > startPriceNew)
+            {
+                return new[] { 0, startPriceOld - startPriceNew };
+            }
+
+            var month = 0;
+            var totalSaving = 0;
+
+            month += 1;
+            totalSaving += savingPerMonth;
+            startPriceOld = (int)(startPriceOld * (1 - (percentLossByMonth / 100)));
+            startPriceNew = (int)(startPriceNew * (1 - (percentLossByMonth / 100)));
+            var left = totalSaving + startPriceOld - startPriceNew;
+
+            return new[] { month, left };
         }
     }
 }
--- BuyingACar/CarTest.cs
+++ BuyingACar/CarTest.cs
@@ -12,5 +12,12 @@ namespace BuyingACar
             var result = new[] { 0, 2000 };
             CollectionAssert.AreEqual(result, BuyCar.NbMonths(10000, 8000, 1000, 1.5));
         }
+
+        [TestMethod]
+        public void OldPrice_2000_NewPrice_8000_At_First_Month_Will_Return_Month_1_Left_Nevigate4910()
+        {
+            var result = new[] { 1, -4910 };
+            CollectionAssert.AreEqual(result, BuyCar.NbMonths(2000, 8000, 1000, 1.5));
+        }
     }
 }
