5ed596f40fa297d929991dca0fab23c2e40ed0f1
gary.lai99 <gary.lai99@TI03NB133>
2019-03-31 16:23:09 +0800

    Add TestMethod "OldPrice_10000_NewPrice_8000_Will_Return_Month_0_Left_2000"

---
+++ BuyingACar/BuyCar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuyingACar
+{
+    public class BuyCar
+    {
+        public static int[] NbMonths(int startPriceOld, int startPriceNew, int savingPerMonth, double percentLossByMonth)
+        {
+            return new[] { 0, startPriceOld - startPriceNew };
+        }
+    }
+}
---
+++ BuyingACar/CarTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BuyingACar
+{
+    [TestClass]
+    public class CarTest
+    {
+        [TestMethod]
+        public void OldPrice_10000_NewPrice_8000_Will_Return_Month_0_Left_2000()
+        {
+            var result = new[] { 0, 2000 };
+            CollectionAssert.AreEqual(result, BuyCar.NbMonths(10000, 8000, 1000, 1.5));
+        }
+    }
+}
